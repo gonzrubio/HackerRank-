1312d4b6091f5b34884e5b86ffbb44811cbc7e37
Gonzalo Rubio <64271182+gonzrubio@users.noreply.github.com>
2020-06-19 14:16:09 -0700

    Uploaded day 3.

--- 10DaysOfStatistics/Day3_Drawing_Marbles.cpp
+++ 10DaysOfStatistics/Day3_Drawing_Marbles.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 /*
-Q: A bag contains  red marbles and  blue marbles. Then,  marbles are drawn from the bag, at random, without replacement.
+Q: A bag contains 3 red marbles and 4 blue marbles. Then,  marbles are drawn from the bag, at random, without replacement.
 If the first marble drawn is red, what is the probability that the second marble is blue?
 
 A:
