e32415a63662546903aa5a4127c971258af27de0
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-02 11:36:25 -0700

    Compute determinant recursively using minors of submatrices

--- 10DaysOfStatistics/Day9/Day9_Multiple_Linear_Regression/main.cpp
+++ 10DaysOfStatistics/Day9/Day9_Multiple_Linear_Regression/main.cpp
@@ -7,6 +7,7 @@ Fit a linear model that depends on m features plus a constant, based on n differ
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <limits>
 
 using mat2D = std::vector<std::vector<double>> ;
 using vec = std::vector<double> ;
@@ -58,90 +59,47 @@ mat2D matmul(const mat2D &A, const mat2D &B)
     return AB ;
 }
 
-double det(mat2D A)
-{   /*Convert square matrix to upper triangular then multiply diagonal elements to get determinant.*/
-    /* Modified rom https://www.geeksforgeeks.org/determinant-of-a-matrix/ */
+mat2D subMatrix(const mat2D &A, const mat2D::size_type row_del, const mat2D::size_type col_del)
+{
+    // Return a matrix with the i-th row and j-th column deleted.
+    mat2D A_sub(A[0].size()-1, vec(A.size()-1)) ;
 
-    const mat2D::size_type n = A[0].size() ;
-    int num1,num2,det = 1,total = 1; // Initialize result
-    mat2D::size_type index ;
-
-    // temporary array for storing row
-    int temp[n + 1];
-
-    //loop for traversing the diagonal elements
-    for(mat2D::size_type i = 0; i < n; i++)
-    {
-        index = i; // initialize the index
-
-        //finding the index which has non zero value
-        while(A[index][i] == 0 && index < n) {
-            index++;
-
-        }
-        if(index == n) // if there is non zero element
+    mat2D::size_type row_sub{0} ;
+    mat2D::size_type col_sub{0} ;
+    for (mat2D::size_type irow{0} ; irow < A.size() ; irow++)
         {
-            // the determinat of matrix as zero
-            continue ;
-
+            if (irow == row_del) continue ;
+            for (mat2D::size_type icol{0} ; icol < A.size() ; icol++)
+                {
+                    if (icol != col_del) A_sub[row_sub][col_sub++] = A[irow][icol] ;
+                    if (col_sub == A.size()-1)
+                    {
+                        col_sub = 0 ;
+                        row_sub++ ;
+                    }
+                }
         }
-        if(index != i)
-        {
-            //loop for swaping the diagonal element row and index row
-            for(mat2D::size_type j = 0; j < n; j++)
-            {
-                std::swap(A[index][j],A[i][j]);
-            }
-                //determinant sign changes when we shift rows
-                //go through determinant properties
-                det = det*std::pow(-1,index-i);
-       }
-
-       //storing the values of diagonal row elements
-       for(mat2D::size_type j = 0; j < n; j++)
-       {
-           temp[j] = A[i][j];
-
-       }
-       //traversing every row below the diagonal element
-       for(mat2D::size_type j = i+1; j < n; j++)
-       {
-           num1 = temp[i]; //value of diagonal element
-           num2 = A[j][i]; //value of next row element
-
-           //traversing every column of row
-           // and multiplying to every row
-           for(mat2D::size_type k = 0; k < n; k++)
-           {
-               //multiplying to make the diagonal
-               // element and next row element equal
-               A[j][k] = (num1 * A[j][k]) - (num2 * temp[k]);
-
-           }
-           total = total * num1; // Det(kA)=kDet(A);
-           }
-
-    }
-
-    //mulitplying the diagonal elements to get determinant
-    for(mat2D::size_type i = 0; i < n; i++)
-    {
-        det *= A[i][i];
-
-    }
-    return (det/total); //Det(kA)/k=Det(A);
+    return A_sub ;
 }
 
-mat2D comatrix(mat2D A)
+double det(const mat2D &A)
 {
-    return A ;
-}
+    if (A[0].size() != A.size())
+    {
+        std::cout << "Not a square matrix." ;
+        return std::numeric_limits<double>::quiet_NaN();
+    }
 
-mat2D adjugate(mat2D A)
-{
-    // Make own fucntion that computes the transpose of the cofactor matrix.
-    // Helper function to compute the cofactor amtrix.
-    return comatrix(transpose(A)) ;
+    if (A.size() == 2) return A[0][0]*A[1][1]-A[0][1]*A[1][0] ;
+    else
+    {
+        double determinant{0} ;
+        for (mat2D::size_type col{0} ; col < A.size() ; ++col)
+        {
+            determinant += pow(-1,col)*A[0][col]*det(subMatrix(A,0,col)) ; // Do I really need to pass the row?
+        }
+        return determinant ;
+    }
 }
 
 
@@ -159,8 +117,9 @@ int main()
                   {0.87, 0.47} } ;
     //vec Y_obs{109.85, 155.72, 137.66, 76.17, 139.75, 162.6, 151.77} ;
     mat2D tmp = matmul(transpose(X_obs),X_obs) ;
-    printMatrix(adjugate(tmp)) ;
-    //std::cout << "\n" << transpose(tmp)/det(tmp) ;
+    printMatrix(tmp) ;
+    std::cout << "\n" << det(tmp) ;
+    //printMatrix(cofactor(matmul(transpose(X_obs),X_obs))) ;
 
     // Get query/print query
     /*4
