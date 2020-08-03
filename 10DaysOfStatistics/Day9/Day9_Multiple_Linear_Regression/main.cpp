5b0c33d5e940b7f2db3b50e061cb08215669277d
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-08-02 23:22:26 -0700

    Add forecast to C++ code. Complete.

--- 10DaysOfStatistics/Day9/Day9_Multiple_Linear_Regression/main.cpp
+++ 10DaysOfStatistics/Day9/Day9_Multiple_Linear_Regression/main.cpp
@@ -159,10 +159,46 @@ vec matvecmul(const mat2D &A, const vec &x)
     return y ;
 }
 
+void inputXY(mat2D &X_obs, vec &Y_obs)
+{
+    vec::size_type ivec{0} ;
+    for (mat2D::size_type ix{0} ; ix < X_obs.size() ; ix++)
+    {
+        for (mat2D::size_type iy{0} ; iy < X_obs[ix].size() ; iy++ )
+        {
+            if (iy == 0) X_obs[ix][iy] = 1 ;
+            else
+            {
+                std::cin >> X_obs[ix][iy] ;
+                if (iy == X_obs[ix].size()-1) std::cin >> Y_obs[ivec++] ;
+            }
+        }
+    }
+}
+
+void inputXq(mat2D &X_query)
+{
+    for (mat2D::size_type qset{0} ; qset < X_query.size() ; qset++)
+    {
+        for (mat2D::size_type mfeat{0} ; mfeat < X_query[qset].size() ; mfeat++ )
+        {
+            std::cin >> X_query[qset][mfeat] ;
+        }
+    }
+}
+
+double linear_model(const vec &x, const vec &w)
+{
+    double sum = w[0] ;
+    for (vec::size_type elem{0} ; elem < x.size() ; elem++)
+        sum += x[elem]*w[elem+1] ;
+    return sum ;
+}
+
 int main()
 {
     /// Sample hard-coded input.
-    vec Y_obs{109.85, 155.72, 137.66, 76.17, 139.75, 162.6, 151.77} ;
+    /*vec Y_obs{109.85, 155.72, 137.66, 76.17, 139.75, 162.6, 151.77} ;
 
     mat2D X_obs = { {0.18, 0.89},
                   {1.0,  0.26},
@@ -175,17 +211,30 @@ int main()
     mat2D X_query = { {0.49, 0.18},
                       {0.57,  0.83},
                       {0.56, 0.64},
-                      {0.76, 0.18} } ;
+                      {0.76, 0.18} } ;*/
 
-    /// Need to add bias to X_obs. Do When getting user inputs.
-    //int m{2} ; // # of Features.
-    //int n{7} ; // # of Observations.
+    /// Read in inputs.
+    vec::size_type m, n, q ;  // Number of features, measurements and query points.
+    std::cin >> m ;
+    std::cin >> n ;
+
+    mat2D X_obs(n,vec(m+1)) ; // Account for bias.
+    vec Y_obs(n) ;
+    inputXY(X_obs,Y_obs) ;
+
+    std::cin >> q ;
+    mat2D X_query(q,vec(m)) ;
+    inputXq(X_query) ;
 
     /// Analytical Least-squares solution.
     vec w = matvecmul(matmul(inv(matmul(transpose(X_obs),X_obs)),transpose(X_obs)),Y_obs) ;
 
     /// Forecast.
-    vec Y_pred{matvecmul(X_query,w)} ;
+    vec Y_pred(X_query.size()) ;
+    for (mat2D::size_type iq{0} ; iq < q ; iq++)
+    {
+        Y_pred[iq] = linear_model(X_query[iq], w) ;
+    }
 
     printAnswer(Y_pred) ;
 
